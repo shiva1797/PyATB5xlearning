b717bdf2a5a14a86db58009c6440cecf1f8ab5ad
shiva <shivakumars1797@gmail.com>
2024-11-13 11:54:35 +0530

    91124 commit

---
+++ NOV/ex_09112024_literals_variables/Lab0016.py
@@ -0,0 +1,3 @@
+Value =min(2,3,744,8484,747473,939393)
+
+print("max number is :",Value)
---
+++ NOV/ex_09112024_literals_variables/Lab0017.py
@@ -0,0 +1,5 @@
+Value =max(2,3,744,8484,747473,939393)
+
+print("max number is :",Value)
+
+print(type(Value))
