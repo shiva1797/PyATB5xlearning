ea8514ddfae0fd2d904f3b239d8f4653504f3697
shiva <shivakumars1797@gmail.com>
2024-11-15 15:08:32 +0530

    14_nov_changes_task

---
+++ NOV/ex_14112024_if_else/Lab0026.py
@@ -0,0 +1,24 @@
+#Write a program that classifies a triangle based on its side lengths.
+# Given three input values representing the lengths of the sides,
+# determine if the triangle is equilateral (all sides are equal),
+# isosceles (exactly two sides are equal),
+# or scalene (no sides are equal).
+# Use an if-else statement to classify the triangle.
+
+side_a = int(input("enter side a:"))
+side_b = int(input("enter side b:"))
+side_c = int(input("enter side c:"))
+
+if side_a == side_b ==side_c:
+    print("equilateral")
+elif(side_a == side_b != side_c) or (side_a!= side_b == side_c) or (side_a==side_c != side_b):
+    print("isosceles")
+elif(side_a != side_b != side_c):
+    print("scalene")
+
+
+
+
+
+
+
