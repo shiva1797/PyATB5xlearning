0d201b1b8f026bace3ae85792f32d1ad7ccc2524
shiva <shivakumars1797@gmail.com>
2024-11-15 11:25:26 +0530

    11_nov_changes

---
+++ NOV/ex_11112024_operators/Lab0025.py
@@ -0,0 +1,20 @@
+# Take a 2 input from the user
+# Print the Quotient and Remainder
+# 15 ->  num1
+# 2 -> num2
+# Q -> 7
+# R -> 1
+#from NOV.ex_09112024_literals_variables.Lab0018 import usr_data_1
+
+user_data1 = int(input("enter num1"))
+user_data2 = int(input("enter num2"))
+
+Q = user_data1//user_data2
+R = user_data1%user_data2
+print("quetient is",Q)
+print("Remainder is",R)
+
+
+
+
+
