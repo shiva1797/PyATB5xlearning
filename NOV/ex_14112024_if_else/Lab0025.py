b3e3e2863051a56f051071ba47cdb3008d266fb4
shiva <shivakumars1797@gmail.com>
2024-11-15 13:08:05 +0530

    14_nov_changes

---
+++ NOV/ex_14112024_if_else/Lab0025.py
@@ -0,0 +1,11 @@
+marks = int(input("enter marks -----\n"))
+
+if (marks >= 90 and marks <= 100):
+    print("A+ grade")
+elif (marks >= 70 and marks <= 90):
+    print("B grade")
+elif (marks >= 50 and marks <= 70):
+    print("C grade")
+else: (print ("fail"))
+
+
