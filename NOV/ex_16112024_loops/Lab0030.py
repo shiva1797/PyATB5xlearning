cc747d328c042bf6079d7337b2ddbf5a90e2a85f
shiva <shivakumars1797@gmail.com>
2024-11-19 15:31:33 +0530

    16_nov_changes_Loops

---
+++ NOV/ex_16112024_loops/Lab0030.py
@@ -0,0 +1,5 @@
+# for i in range(1, 10):
+# for i in range(10):
+# for i in range(-10, -1, 2):
+for i in range(-1, -10, -1):  #-1 -10-1=-9, -1-1=-2
+    print(i)
---
+++ NOV/ex_16112024_loops/Lab0031.py
@@ -0,0 +1,4 @@
+for i in range(10):  # 0 to 9 , times -> 10
+    print("Hello World!")
+
+    
